5ba6cbfb7d7d7329cb0d33e883b52561ff3f94d9
Kaizhadanche <kaizhadanche@outlook.com>
2022-08-08 22:48:37 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,10 +42,10 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route','/水群各类资源'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:kaizhadanche@outlook.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
@@ -53,11 +53,15 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/kaizhadanche/onedrive-vercel-index',
     },
     {
-      name: 'Telegram',
-      link: 'https://t.me/realSpencerWoo',
+      name: 'QQ',
+      link: 'https://jq.qq.com/?_wv=1027&k=Dektnz7s',
+    },
+    {
+      name: 'Bilibili',
+      link: 'https://space.bilibili.com/410039331',
     },
   ],
 
