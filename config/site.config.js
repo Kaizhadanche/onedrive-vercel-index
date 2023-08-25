d95822af3f941791ce90e38b96f469e4a91928e5
Kaizhadanche <kaizhadanche@outlook.com>
2023-08-25 12:27:16 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route','/水群','/Docs/盛夏的柳树','/YZ'],
+  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route','/Docs/盛夏的柳树','/TZ'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:kaizhadanche@outlook.com',
